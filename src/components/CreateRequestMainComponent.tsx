904524acb1b8c2d742392078d2ca7357a8ce9b68
MortenssenDiego <mortenssendiego@gmail.com>
2024-10-23 22:51:37 -0300

    unused imports cleanup

--- src/components/CreateRequestMainComponent.tsx
+++ src/components/CreateRequestMainComponent.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Modal, Button, TextInput } from "flowbite-react";
+import React from "react";
+import { Button } from "flowbite-react";
 import { HiPlus, HiX } from "react-icons/hi";
 
 interface CreateRequestMainComponentProps {
